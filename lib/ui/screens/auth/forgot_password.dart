83ee10145591e8fc79561ad8c00cb5881d609546
Mubarak Abdulrasak <101928047+halland1109@users.noreply.github.com>
2023-09-30 04:50:43 +0100

    forget password screen and reset password screen

--- lib/ui/screens/auth/forgot_password.dart
+++ lib/ui/screens/auth/forgot_password.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:translator_ai/ui/components/custom_button.dart';
+import 'package:translator_ai/utils/colors.dart';
 
 class ForgotPassword extends StatefulWidget {
   const ForgotPassword({super.key});
@@ -8,8 +11,54 @@ class ForgotPassword extends StatefulWidget {
 }
 
 class _ForgotPasswordState extends State<ForgotPassword> {
+  final TextEditingController _emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
+            const SizedBox(height: 60),
+            const Text(
+              "Forgot Your Password?",
+              style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.primary),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              "Enter the Email Address associated with your account",
+              style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.blackColor),
+            ),
+            const SizedBox(height: 30),
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "Enter your email address",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            CustomButton(
+                width: 12,
+                // i have a question here
+                height: 12,
+                color: AppColors.blackColor,
+                content: "content",
+                onTap: () {})
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/ui/screens/auth/reset_password.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:translator_ai/utils/colors.dart';
+
+class ResetPassword extends StatefulWidget {
+  @override
+  _ResetPasswordState createState() => _ResetPasswordState();
+}
+
+class _ResetPasswordState extends State<ResetPassword> {
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmNewPasswordController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
+            const SizedBox(height: 60),
+            const Text(
+              'Reset Password',
+              style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.primary),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              'Now you can reset your new password',
+              style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.blackColor),
+            ),
+            TextField(
+              controller: _newPasswordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'New Password',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _confirmNewPasswordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Confirm New Password',
+              ),
+            ),
+            SizedBox(height: 16.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
