5a6f59f995f20b76678df006d4c16759d0b9aae8
Mubarak Abdulrasak <101928047+halland1109@users.noreply.github.com>
2023-10-01 05:14:27 +0100

    rasakmubarak

--- lib/ui/screens/auth/forgot_password.dart
+++ lib/ui/screens/auth/forgot_password.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:translator_ai/ui/components/custom_button.dart';
 import 'package:translator_ai/utils/colors.dart';
+import 'package:translator_ai/utils/size_calculator.dart';
 
 class ForgotPassword extends StatefulWidget {
   const ForgotPassword({super.key});
@@ -19,42 +21,52 @@ class _ForgotPasswordState extends State<ForgotPassword> {
         backgroundColor: AppColors.whiteColor,
         body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
+            padding:
+                EdgeInsets.symmetric(horizontal: sizer(false, 30, context)),
             child: Column(
               children: [
-                const SizedBox(height: 60),
-                const Text(
+                SizedBox(
+                  height: sizer(false, 60, context),
+                ),
+                Text(
                   "Forgot Your Password?",
-                  style: TextStyle(
-                      fontSize: 34,
+                  style: GoogleFonts.nunito(
+                      fontSize: 20,
                       fontWeight: FontWeight.bold,
                       color: AppColors.primary),
                 ),
-                const SizedBox(height: 15),
-                const Text(
+                SizedBox(height: sizer(false, 15, context)),
+                Text(
                   "Enter the Email Address associated with your account",
-                  style: TextStyle(
-                      fontSize: 34,
+                  style: GoogleFonts.nunito(
+                      fontSize: sizer(true, 14, context),
                       fontWeight: FontWeight.w600,
                       color: AppColors.blackColor),
+                  maxLines: 3,
                 ),
                 const SizedBox(height: 30),
                 TextField(
                   controller: _emailController,
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
                     hintText: "Enter your email address",
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+                      borderRadius: BorderRadius.circular(20),
                     ),
                   ),
                 ),
-                const SizedBox(height: 20),
+                SizedBox(
+                  height: sizer(false, 30, context),
+                ),
                 CustomButton(
-                    width: 12,
+                    width: sizer(true, 212, context),
                     // i have a question here
-                    height: 12,
-                    color: AppColors.blackColor,
+                    height: sizer(false, 50, context),
+                    color: AppColors.primary,
                     content: "Verify Email",
                     onTap: () {})
               ],
