2bbc1dfe85a8bdd3ca9eb8beff7e03fce165b790
Mubarak Abdulrasak <101928047+halland1109@users.noreply.github.com>
2023-10-01 10:23:44 +0100

    lastest changes

--- lib/ui/screens/auth/onBoardingPage.dart
+++ lib/ui/screens/auth/onBoardingPage.dart
@@ -11,42 +11,51 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body : SafeArea(
+      body: SafeArea(
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-
             children: [
               Container(
                   height: MediaQuery.of(context).size.height,
-
                   child: Padding(
-                    padding:  EdgeInsets.all(30.0),
+                    padding: EdgeInsets.all(30.0),
                     child: Center(
                       child: Column(
                         children: [
                           Image.asset('images/splash.jpeg'),
-                          const SizedBox(height: 30,),
-                          const Text('Your Passport To Global Communication '
-                            ,style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold),),
-                          const SizedBox(height: 30,),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          const Text(
+                            'Your Passport To Global Communication ',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
                           // const Text('Get Started Now!!!',style: TextStyle(
                           //     fontSize: 20,
                           //     fontWeight: FontWeight.bold),),
                           // const SizedBox(height: 30,),
-                          const Text('Your Multilingual Companion.'
-                            ,style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.bold),),
-                          SizedBox(height: 30,),
-                          const Text('Get Started Now!!!',style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),),
-                          const SizedBox(height: 30,),
+                          const Text(
+                            'Your Multilingual Companion.',
+                            style: TextStyle(
+                                fontSize: 17, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          const Text(
+                            'Get Started Now!!!',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
                           CustomButton(
                               width: sizer(true, 213, context),
                               height: 51,
@@ -55,10 +64,12 @@ class OnBoardingScreen extends StatelessWidget {
                               color: AppColors.primary,
                               content: 'Create an Account',
                               onTap: () {
-                                Navigator.of(context).pushNamed(RouteHelper.signUpRoute);
-
+                                Navigator.of(context)
+                                    .pushNamed(RouteHelper.signUpRoute);
                               }),
-                          SizedBox(height: 30,),
+                          SizedBox(
+                            height: 30,
+                          ),
                           CustomButton(
                               width: sizer(true, 213, context),
                               height: 51,
@@ -67,7 +78,8 @@ class OnBoardingScreen extends StatelessWidget {
                               color: AppColors.primary,
                               content: 'Login',
                               onTap: () {
-                                Navigator.of(context).pushNamed(RouteHelper.loginRoute);
+                                Navigator.of(context)
+                                    .pushNamed(RouteHelper.settingsRoute);
                               }),
                         ],
                       ),
